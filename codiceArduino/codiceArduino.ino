3a5e687ff16f4b284632963c18d189031902fd64
Paolo Geroli <paologeroli2006@gmail.com>
2024-05-17 10:39:25 +0200

    Aggiunta funzioni fondamentali

--- codiceArduino/codiceArduino.ino
+++ codiceArduino/codiceArduino.ino
@@ -1,7 +1,40 @@
+#define PIN_INCLINAZIONE
+#define PIN_RELE
+#define PIN_PANNELLO
+#define PIN_STEPPER
+#define PIN_SERVO
+
+
 void setup() {
 
 }
 
 void loop() {
+  
+}
+
+
+
+
+
+void cercaLuce(){
 
 }
+
+void calcolaAngolazione(){
+
+
+}
+
+void ruota(int posizione){
+
+}
+
+void scrivi(){
+
+}
+
+void inclina(){
+
+}
+
