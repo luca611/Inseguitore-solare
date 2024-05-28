c0bdbb1dfcc01cf2fa4d433ffdfabf3a9524d58f
Paolo Geroli <paologeroli2006@gmail.com>
2024-05-28 09:36:26 +0200

    sistemo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, request
 app=Flask("Sunflower")
 
-@app.route("/<path:p")
+@app.route("/<path:p>",methods=['GET','POST'])
 def serve_static(p):
     return app.send_static_file(p)
 
